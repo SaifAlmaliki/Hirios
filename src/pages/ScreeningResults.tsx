7799507c3135942c12b7b8b6b4a8b5d87a6b30a0
Saif Almaliki <saif.wsm@gmail.com>
2025-10-24 10:20:09 +0200

    refactor: simplify combined filters section in ScreeningResults component by removing unnecessary CardHeader

--- src/pages/ScreeningResults.tsx
+++ src/pages/ScreeningResults.tsx
@@ -363,13 +363,7 @@ const ScreeningResults = () => {
 
           {/* Combined Filters */}
           <Card className="mt-8">
-            <CardHeader>
-              <CardTitle className="text-lg flex items-center">
-                <Filter className="h-5 w-5 mr-2" />
-                Filters & Search
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
+            <CardContent className="pt-6">
               <div className={`grid grid-cols-1 md:grid-cols-2 gap-4 ${jobId ? 'lg:grid-cols-2' : 'lg:grid-cols-3'}`}>
                 {/* Job Title Filter - only show if not viewing specific job */}
                 {!jobId && (
